15f2c29d17f3d2ca2bd5a296f8ad1547828585a7
hqwy4 <hbw3.gustavo@gmail.com>
2024-05-22 11:47:06 -0300

    filter

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -18,7 +18,22 @@ module.exports = class IndexController {
             const count = repositories.length
 
 
-            const works = await fetchForPortfolio();
+            const filter = req.query.category || '';
+            let works = await fetchForPortfolio();
+            console.log(filter)
+
+            if (filter === "") {
+                works = works;
+            } else if (filter == 1) {
+                works = works.filter(work => work.type === 1)
+            } else if (filter == 2) {
+                works = works.filter(work => work.type === 2)
+            } else if (filter == 3) {
+                works = works.filter(work => work.type === 3)
+            }
+
+            console.log("==============")
+            // console.log(works)
 
             return res.render("layouts/main", { router: router, allWorks: count, works: works })
         } catch (error) {
